63212e27f789a830bf449b40561a5234d988680a
vara <vara@gmail.com>
2017-12-11 16:31:51 +0530

    initial commit for vegsho

---
+++ vegshop/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('vegshop App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
---
+++ vegshop/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAWuCKDi-LaVgQmb-jvbbWLWNQ15N3T0qg",
+    authDomain: "vegshop-d7371.firebaseapp.com",
+    databaseURL: "https://vegshop-d7371.firebaseio.com",
+    projectId: "vegshop-d7371",
+    storageBucket: "",
+    messagingSenderId: "511965155553"
+  }
+};
